a81a294db72b5c76c97fce8a2a5a8161284ddc9b
Long Huynh <long.l.huynh@gmail.com>
2022-05-31 21:22:03 -0400

    finished user-authentication using email verification and password

---
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -0,0 +1,107 @@
+package com.example.myapplication
+
+import android.app.ProgressDialog
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
+import com.example.myapplication.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+
+    //ViewBinding
+    private lateinit var binding:ActivityLoginBinding
+
+    //ActionBar
+    private lateinit var actionBar: ActionBar
+
+    //Progress Dialog
+    private lateinit var progressDialog: ProgressDialog
+
+    //Firebase Authentication
+    private lateinit var firebaseAuth: FirebaseAuth
+    private var email = ""
+    private var password = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //configure actionbar
+        actionBar = supportActionBar!!
+        actionBar.title = "Login"
+
+        //configure progress dialog
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Please wait")
+        progressDialog.setMessage("Logging In...")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        //firebase authentication init
+        firebaseAuth = FirebaseAuth.getInstance()
+        checkUser()
+
+        //handle registering button
+        binding.noAccountTv.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
+        //handling the sign in button
+        binding.loginBtn.setOnClickListener{
+            //validate data before login in
+            validateData()
+        }
+    }
+    private fun validateData(){
+        //get data
+        email = binding.emailEt.text.toString().trim()
+        password = binding.passwordEt.text.toString().trim()
+
+        //validate data
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            //invalid email format
+            binding.emailEt.error = "Invalid email format"
+        }else if(TextUtils.isEmpty(password)){
+            //if no password entered
+            binding.passwordEt.error = "Please enter password"
+        }else{
+            //data validated
+            firebaseLogin()
+        }
+    }
+    private fun firebaseLogin(){
+        //show progress
+        progressDialog.show()
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnSuccessListener {
+            //when login successful
+            progressDialog.dismiss()
+            //get user info
+            val firebaseUser = firebaseAuth.currentUser
+            val email = firebaseUser!!.email
+            Toast.makeText(this, "Login as $email", Toast.LENGTH_SHORT).show()
+
+            //open profile
+            startActivity(Intent(this, ProfileActivity::class.java))
+            finish()
+        }.addOnFailureListener { e->
+            //when login fail
+            progressDialog.dismiss()
+            Toast.makeText(this, "${e.message}", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun checkUser(){
+        //if user already logged in, go straight to profile activity
+        //get current user
+        val firebaseUser = firebaseAuth.currentUser
+        //if user is already logged in
+        if(firebaseUser != null){
+            startActivity(Intent(this, ProfileActivity::class.java))
+            finish()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/SignUpActivity.kt
@@ -0,0 +1,106 @@
+package com.example.myapplication
+
+import android.app.ProgressDialog
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
+import com.example.myapplication.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class SignUpActivity : AppCompatActivity() {
+    //ViewBinding
+    private lateinit var binding: ActivitySignUpBinding
+
+    //ActionBar
+    private lateinit var actionBar: ActionBar
+
+    //ProgressDialog
+    private lateinit var progressDialog: ProgressDialog
+
+    //FirebaseAuth
+    private lateinit var firebaseAuth: FirebaseAuth
+    private var email = ""
+    private var password = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Configure Actionbar
+        actionBar = supportActionBar!!
+        actionBar.title = "Sign Up"
+        actionBar.setDisplayHomeAsUpEnabled(true)
+        actionBar.setDisplayShowHomeEnabled(true)
+
+        //configure progress dialog
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Please wait")
+        progressDialog.setMessage("Creating account...")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        //firebase authentication init
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        //sign up button listener
+        binding.signUpBtn.setOnClickListener {
+            //validate data
+            validateData()
+        }
+
+    }
+
+    private fun validateData(){
+        //get data
+        email = binding.emailEt.text.toString().trim()
+        password = binding.passwordEt.text.toString().trim()
+
+        //validate data
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            //invalid email format
+            binding.emailEt.error = "Invalid email format"
+        }else if(TextUtils.isEmpty(password)){
+            //if no password entered
+            binding.passwordEt.error = "Please enter password"
+        }else if(password.length < 6){
+            binding.passwordEt.error = "Password must be at least 6 characters"
+        }else{
+            //sign up accepted
+            firebaseSignUp()
+        }
+    }
+
+    private fun firebaseSignUp(){
+        //show progress
+        progressDialog.show()
+
+        //create an account
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnSuccessListener {
+            //successful signup
+            progressDialog.dismiss()
+
+            //get user
+            val firebaseUser = firebaseAuth.currentUser
+            val email = firebaseUser!!.email
+            Toast.makeText(this, "Account created with email $email", Toast.LENGTH_SHORT).show()
+
+            //open profile
+            startActivity(Intent(this, ProfileActivity::class.java))
+            finish()
+        }.addOnFailureListener{e->
+            progressDialog.dismiss()
+            Toast.makeText(this, "${e.message}", Toast.LENGTH_SHORT).show()
+
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean{
+        //go back to previous activity when pressed
+        onBackPressed()
+        return super.onSupportNavigateUp()
+    }
+}
